4fb3053f47b697259a32e34c65922011b6b33061
XCELSOR <xcelsor20@gmail.com>
2020-05-06 18:41:55 +0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,109 @@
+def wtf(a, b):
+    answer = a * b
+    return answer
+
+
+def devis(a, b):
+
+    anser = a / b
+
+    return anser
+
+def add(a, b):
+    anser = a + b
+
+    return anser
+
+def sub(a, b):
+    anser = a - b
+
+    return anser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
